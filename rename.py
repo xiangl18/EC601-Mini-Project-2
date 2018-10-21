1b8332e21ff7d9022f73a7935e97b146868c9380
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2018-10-21 16:24:47 -0400

    Update rename.py

--- rename.py
+++ rename.py
@@ -11,7 +11,10 @@ for index, name in enumerate(classes):
         if item.endswith('.jpg'):
             os.rename(os.path.join(class_path, item), os.path.join(class_path, (name + '.' + str(i) + '.jpg')))
             filePath = os.path.join(class_path, (name + '.' + str(i) + '.jpg'))
-            newfile = base_path + '/images/' + name + '.' + str(i) + '.jpg'
+            output_path = os.path.join(base_path, 'images/')
+            if (not os.path.exists(output_path)):
+                os.mkdir(output_path)
+            newfile = output_path + name + '.' + str(i) + '.jpg'
             shutil.copyfile(filePath, newfile)
             i = i + 1
         else:
