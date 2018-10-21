7552b965c10f02a3704d0c57c405f029aa288ac8
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2018-10-21 15:50:33 -0400

    Add files via upload
    Do data augmentation, and get train and validation data flow.
    There are some parameters which could be set here.

---
+++ get_DataFlow.py
@@ -0,0 +1,29 @@
+from keras.preprocessing.image import ImageDataGenerator
+import os
+
+
+BATH_SIZE = 32
+EPOCHS = 50
+STEPS_PER_EPOCH = 100
+
+base_path = os.getcwd()
+path = os.path.join(base_path, 'dataset/')
+train_dir = os.path.join(path, 'train/')
+val_dir = os.path.join(path, 'validation/')
+test_dir = os.path.join(path, 'validation/')
+output_model = os.path.join(base_path, 'model_output')
+
+
+if (not os.path.exists(output_model)):
+                    os.mkdir(output_model)
+
+train_pic_gen = ImageDataGenerator(rescale=1./255, rotation_range=20, width_shift_range=0.2, height_shift_range=0.2,
+                                     shear_range=0.2, zoom_range=0.5, horizontal_flip=True, fill_mode='nearest')
+
+test_pic_gen = ImageDataGenerator(rescale=1./255)
+
+train_flow = train_pic_gen.flow_from_directory(train_dir, (128, 128), batch_size = BATH_SIZE, class_mode='binary')
+
+val_flow = test_pic_gen.flow_from_directory(val_dir, (128,128), batch_size = BATH_SIZE, class_mode='binary')
+
+test_flow = test_pic_gen.flow_from_directory(test_dir, (128,128), batch_size = BATH_SIZE, class_mode='binary')
