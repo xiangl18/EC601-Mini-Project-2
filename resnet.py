7adb0dbb47e5e76f1d3bb369cb429d84167b5059
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2018-10-22 14:09:41 -0400

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -16,7 +16,7 @@ model.add(layers.Dense(2, activation='softmax'))
 
 model.summary()
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 model.fit_generator(
       df.train_flow,
